adbe73dfc8c408c4ef2daeae4f6ccb009ee25990
williamdam123 <143433814+williamdam123@users.noreply.github.com>
2026-01-13 18:53:45 -0800

    Create Player class with name and symbol attributes

---
+++ Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    private final String name;
+    private final char symbol;
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
